ddd8818db0788241c4589a4ff0cefea13be7931a
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-01 16:40:20 -0300

    Execução do Case 1

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,20 +1,25 @@
 package candidatura;
 
+import java.util.Random;
+
 public class ProcessoSeletivo {
   public static void main(String[] args) throws Exception {
+    double salarioRandom = new Random().nextDouble(1500.0, 2500.0);
       analisarCandidato(1900.0);
       analisarCandidato(2200.0);
       analisarCandidato(2000.0);
+      System.out.printf("O salário do último canditato é igual: %.2f",  salarioRandom);
+      analisarCandidato(salarioRandom);
       
   }
   static void analisarCandidato(double salarioPretendido){
       double salarioBase = 2000.00;
       if (salarioBase > salarioPretendido) {
-          System.out.println("LIGAR PARA O CANDIDATO");
+          System.out.println(" LIGAR PARA O CANDIDATO");
       } else if (salarioBase == salarioPretendido) {
-          System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+          System.out.println(" LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
       } else {
-      System.out.println("AGUARDANDO RESULTADO DEMAIS CANDIDATOS");
+      System.out.println(" AGUARDANDO RESULTADO DEMAIS CANDIDATOS");
     }
   }
 }
